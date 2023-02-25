e80e8fc10b96dbb8206a4ac39405bc508b2a45c3
NitroGuy10 <naviyer10@gmail.com>
2023-02-25 11:11:55 -0600

    Better platform creation

--- assets/Objects/WordPlatformInteractableStaticBody2D.gd
+++ assets/Objects/WordPlatformInteractableStaticBody2D.gd
@@ -11,15 +11,26 @@ export var playSound = false
 
 var pressed = false
 var text
+var textBounds = Vector2(0, 0)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	text = get_parent().get_parent().get_node("WordPlatformText")
+	
+	# Set the CollisionShape2D sizes to that of the text
+	var richTextLabel = get_parent().get_parent().get_node("WordPlatformText/RichTextLabel")
+	richTextLabel.text = get_parent().get_parent().textString
+	textBounds = richTextLabel.get_font("normal_font").get_string_size(richTextLabel.text) / 2
+	$CollisionShape2D.shape.extents = textBounds  # Both collisionboxes use the same shape, so we only gotta change the shape once
+	$CollisionShape2D.position = Vector2(textBounds.x, textBounds.y - 10)
+	get_parent().get_node("StaticBody2D/CollisionShape2D").position = textBounds
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	if pressed:
 		text.position.y = lerp(text.position.y, 0, 0.2)
 	else:

e7e24fc341eb76d853434bf1a016b5fef388c39d
NitroGuy10 <naviyer10@gmail.com>
2023-02-25 12:32:07 -0600

    Dash platform

--- assets/Objects/PlayerMovement.gd
+++ assets/Objects/PlayerMovement.gd
@@ -7,10 +7,13 @@ extends KinematicBody2D
 var isPlayer2 = false
 
 var jumpvelocity = 800.0
+var dashvelocity = 1000.0
+var dashHorizScalar = 2.5
 var happyJumpVelocity = 1200.0
 var walkspeed = 100.0
 var horizontal_damping = 0.8
 var gravityscale = 1600.0
+var canDash = false
 
 var currentEmotion = "none"
 
@@ -26,9 +29,14 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if currentEmotion == "happy":
-		$CPUParticles2D.emitting = true
+		$HappyParticles.emitting = true
 	else:
-		$CPUParticles2D.emitting = false
+		$HappyParticles.emitting = false
+	
+	if currentEmotion == "excited" and canDash:
+		$DashParticles.emitting = true
+	else:
+		$DashParticles.emitting = false
 		
 
 func is_pressed_for_me(key_name: String):
@@ -36,6 +44,12 @@ func is_pressed_for_me(key_name: String):
 		return Input.is_action_pressed(key_name + "_p2")
 	else:
 		return Input.is_action_pressed(key_name)
+
+func is_just_pressed_for_me(key_name: String):
+	if isPlayer2:
+		return Input.is_action_just_pressed(key_name + "_p2")
+	else:
+		return Input.is_action_just_pressed(key_name)
 
 func _physics_process(delta):
 	if is_pressed_for_me("ui_left"):
@@ -47,14 +61,29 @@ func _physics_process(delta):
 	
 	
 	if is_on_floor():
+		if currentEmotion == "excited":
+			canDash = true
 		if is_pressed_for_me("ui_up"):
 			if currentEmotion == "happy":
 				velocity.y = -happyJumpVelocity
 			else:
-				velocity.y = -jumpvelocity				
+				velocity.y = -jumpvelocity
 		else:
 			velocity.y = 0
 	else:
 		velocity.y += gravityscale * delta
+		if canDash and is_pressed_for_me("dash"):
+			canDash = false
+			if is_pressed_for_me("ui_up"):
+				velocity.y = -dashvelocity
+			elif is_pressed_for_me("ui_down"):
+				velocity.y = dashvelocity
+			elif is_pressed_for_me("ui_left"):
+				velocity.x = -dashvelocity * dashHorizScalar
+			elif is_pressed_for_me("ui_right"):
+				velocity.x = dashvelocity * dashHorizScalar
+			else:
+				canDash = true
+			
 
 	move_and_slide(velocity, Vector2.UP)

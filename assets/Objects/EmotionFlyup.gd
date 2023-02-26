55545aaf459473a0673b7db5f05f9ced3d701a1e
NitroGuy10 <naviyer10@gmail.com>
2023-02-26 07:00:14 -0600

    Emotion flyup text

--- assets/Objects/EmotionFlyup.gd
+++ assets/Objects/EmotionFlyup.gd
@@ -7,16 +7,16 @@ onready var player_vars = get_node("/root/PlayerVariables")
 # var b = "text"
 
 var EMOTIONS = {
-	"none": { "color": Color(1, 1, 1), "text": "" },
-	"happiness": { "color": Color(1, 1, 0), "text": "Joy" },
-	"fear": { "color": Color(0.72, 0, 1), "text": "Fear" },
-	"anger": { "color": Color(1, 0, 0), "text": "Anger" },
-	"disgust": { "color": Color(0, 1, 0), "text": "Disgust" },
-	"contempt": { "color": Color(0, 0, 0), "text": "Contempt" },
-	"guilt": { "color": Color(0, 1, 1), "text": "Guilt" },
-	"distress": { "color": Color(0.5, 0.5, 0.5), "text": "Distress" },
-	"peace": { "color": Color(0, 0, 0.5), "text": "Peace" },
-	"hope": { "color": Color(1, 0.5, 0), "text": "Hope" }
+	"none": { "color": Color(1, 1, 1, 1), "text": "" },
+	"happiness": { "color": Color(1, 1, 0, 1), "text": "Joy" },
+	"fear": { "color": Color(0.72, 0, 1, 1), "text": "Fear" },
+	"anger": { "color": Color(1, 0, 0, 1), "text": "Anger" },
+	"disgust": { "color": Color(0, 1, 0, 1), "text": "Disgust" },
+	"contempt": { "color": Color(0, 0, 0, 1), "text": "Contempt" },
+	"guilt": { "color": Color(0, 1, 1, 1), "text": "Guilt" },
+	"distress": { "color": Color(0.5, 0.5, 0.5, 1), "text": "Distress" },
+	"peace": { "color": Color(0, 0, 0.5, 1), "text": "Peace" },
+	"hope": { "color": Color(1, 0.5, 0, 1), "text": "Hope" }
 }
 
 var lastEmotion = "none"
@@ -31,5 +31,8 @@ func _process(delta):
 	if player_vars.currentEmotion != lastEmotion:
 		var emotion = EMOTIONS[player_vars.currentEmotion]
 		$WordPlatformText/RichTextLabel.bbcode_text = "[center]" + emotion.text + "[/center]"
+		$WordPlatformText/RichTextLabel.set("custom_colors/default_color", emotion.color)
+		lastEmotion = player_vars.currentEmotion
 		
-		
+	$WordPlatformText/RichTextLabel.set("custom_colors/default_color", lerp($WordPlatformText/RichTextLabel.get("custom_colors/default_color"), Color(1, 1, 1, 0), 0.9 * delta))
+	

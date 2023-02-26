0959f1143b6d27a4e5c7aa4a6e8e5f87bd95ebae
NitroGuy10 <naviyer10@gmail.com>
2023-02-26 07:16:31 -0600

    Cool background fx

--- assets/Objects/Background.gd
+++ assets/Objects/Background.gd
@@ -6,12 +6,24 @@ onready var player_vars = get_node("/root/PlayerVariables")
 # var a = 2
 # var b = "text"
 
+var initialPos
+var offsetPos = Vector2(0, 0)
+
+onready var player1KB = get_parent().get_node("Player/PlayerKinematicBody2D")
+onready var player2KB = get_parent().get_node("Player2/PlayerKinematicBody2D")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	initialPos = position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	visible = player_vars.currentEmotion != "contempt"
+	
+	offsetPos = (player1KB.global_position + player2KB.global_position) / 2.0
+	offsetPos.x -= 640
+	offsetPos.y -= 360
+	offsetPos *= 0.05
+	position = lerp(position, initialPos + offsetPos, 5 * delta)

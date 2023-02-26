f9c839cf732686ec30a95cd8b63a9c5c1e16ca8e
NitroGuy10 <naviyer10@gmail.com>
2023-02-26 10:26:59 -0600

    The end

---
+++ assets/Levels/TheEnd.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Timer.start()
+	$AnimatedSprite.play()
+	$AudioStreamPlayer.play()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Timer_timeout():
+	$WordPlatformText.visible = true

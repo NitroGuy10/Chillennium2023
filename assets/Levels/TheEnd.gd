acf032c87c881acc3edb450a17c4a9b1d2b4a63d
NitroGuy10 <naviyer10@gmail.com>
2023-02-26 13:21:50 -0600

    t

--- assets/Levels/TheEnd.gd
+++ assets/Levels/TheEnd.gd
@@ -9,6 +9,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Timer.start()
+	$Timer2.start()	
 	$AnimatedSprite.play()
 	$AudioStreamPlayer.play()
 	pass # Replace with function body.
@@ -21,3 +22,8 @@ func _ready():
 
 func _on_Timer_timeout():
 	$WordPlatformText.visible = true
+
+
+func _on_Timer2_timeout():
+	$AnimatedSprite2.visible = true
+	$AudioStreamPlayer2.play()
